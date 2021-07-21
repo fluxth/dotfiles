7e4dccefbe452d287381cb5966161334192b5112
fluxTH <flux@fluxthitat.me>
2021-07-22 02:46:53 +0700

    nvim: add yaml to treesitter requirements

--- editor/.config/nvim/lua/fluxth/treesitter.lua
+++ editor/.config/nvim/lua/fluxth/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = {
-    "c", "cpp", "python", "rust", "toml", "dockerfile",
+    "c", "cpp", "python", "rust", "toml", "dockerfile", "yaml",
     "javascript", "typescript", "tsx", "css", "scss", "html", "json", "jsonc"
   },
   highlight = {
