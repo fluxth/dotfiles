bf65cc7512d584a0540a697b36fe4bf4dfca472f
fluxTH <flux@fluxthitat.me>
2021-07-22 19:45:43 +0700

    make: add meslo nerdfonts to linux install pipeline

--- scripts/install_archlinux.sh
+++ scripts/install_archlinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -Syyu neovim alacritty mpv zsh tmux rustup nodejs npm python-pip
+sudo pacman -Syyu neovim alacritty mpv zsh tmux rustup nodejs npm python-pip ttf-meslo-nerd-font-powerlevel10k
