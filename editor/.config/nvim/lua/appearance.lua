23382eadee3b22df462e80f42215499736fedcf3
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-07-27 19:24:19 +0700

    nvim: fix appearance.lua highlights not sourcing

--- editor/.config/nvim/lua/appearance.lua
+++ editor/.config/nvim/lua/appearance.lua
@@ -1,14 +1,21 @@
 -- set colorscheme to gruvbox
 vim.g.gruvbox_contrast_dark = 'hard'
 vim.cmd('colorscheme gruvbox')
-vim.cmd('highlight Normal guibg=none')  -- make bg transparent
+
+-- highlight helper function
+local function highlight(opt)
+    vim.cmd([[autocmd VimEnter * highlight ]] .. opt)
+end
+
+-- make bg transparent
+highlight('Normal guibg=none')
 
 -- set custom colors for various plugins
-vim.cmd('highlight link Blamer GruvboxBg2')  -- make git blames more subtle
-vim.cmd('highlight link CocHintSign GruvboxBg2')  -- make rust inlay hints more subtle
-vim.cmd('highlight link GitSignsChange GruvboxOrangeSign')  -- orange gitsign when line is changed
+highlight('link Blamer GruvboxBg2')  -- make git blames more subtle
+highlight('link CocHintSign GruvboxBg2')  -- make rust inlay hints more subtle
+highlight('link GitSignsChange GruvboxOrangeSign')  -- orange gitsign when line is changed
 
 -- coc diagnostic styles on signcolumn
-vim.cmd('highlight link CocErrorSign GruvboxRedBold')
-vim.cmd('highlight link CocWarningSign GruvboxOrange')
-vim.cmd('highlight link CocInfoSign GruvboxBlue')
+highlight('link CocErrorSign GruvboxRedBold')
+highlight('link CocWarningSign GruvboxOrange')
+highlight('link CocInfoSign GruvboxBlue')
