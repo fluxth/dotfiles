ec6a6de9a248cd8f9d8bda8dcf37c77f4111f33b
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-07-27 14:57:42 +0700

    nvim: use separate option tables in binds.lua

--- editor/.config/nvim/lua/binds.lua
+++ editor/.config/nvim/lua/binds.lua
@@ -4,64 +4,62 @@ local map = vim.api.nvim_set_keymap
 map('n', '<space>', '', {})
 vim.g.mapleader = ' '
 
-options = { noremap = true }
-
 -- esc is hard to reach
-map('i', 'jk', '<esc>', options)
+map('i', 'jk', '<esc>', { noremap = true, silent = true })
 
 -- file manipulation
-map('n', '<leader>q', ':q<cr>', options)
-map('n', '<leader>Q', ':q!<cr>', options)
-map('n', '<leader>w', ':w<cr>', options)
-map('n', '<leader>x', ':wq<cr>', options)
+map('n', '<leader>q', ':q<cr>', { noremap = true })
+map('n', '<leader>Q', ':q!<cr>', { noremap = true })
+map('n', '<leader>w', ':w<cr>', { noremap = true })
+map('n', '<leader>x', ':wq<cr>', { noremap = true })
 
 -- pane navigation
-map('n', '<leader>h', '<c-w>h', options)
-map('n', '<leader>j', '<c-w>j', options)
-map('n', '<leader>k', '<c-w>k', options)
-map('n', '<leader>l', '<c-w>l', options)
-map('n', '<leader>p', '<c-w>p', options)
-map('n', '<leader>=', '<c-w>=', options) 
+map('n', '<leader>h', '<c-w>h', { noremap = true })
+map('n', '<leader>j', '<c-w>j', { noremap = true })
+map('n', '<leader>k', '<c-w>k', { noremap = true })
+map('n', '<leader>l', '<c-w>l', { noremap = true })
+map('n', '<leader>p', '<c-w>p', { noremap = true })
+map('n', '<leader>=', '<c-w>=', { noremap = true }) 
 
 -- escape from terminal insert mode
-map('t', '<leader>\\', '<c-\\><c-n>', options)
+map('t', '<leader>\\', '<c-\\><c-n>', { noremap = true })
 
 -- close buffer
-map('n', '<leader>d', ':bdcr>', options)
-map('n', '<leader>D', ':bd!<cr>', options)
+map('n', '<leader>d', ':bdcr>', { noremap = true })
+map('n', '<leader>D', ':bd!<cr>', { noremap = true })
 
 -- buffer navigation
-map('n', '<leader><leader>', '<c-^>', options)
-map('n', '<c-k>', ':bp<cr>', options)
-map('n', '<c-j>', ':bn<cr>', options)
+map('n', '<leader><leader>', '<c-^>', { noremap = true, silent = true })
+map('n', '<c-k>', ':bp<cr>', { noremap = true })
+map('n', '<c-j>', ':bn<cr>', { noremap = true })
 
 -- tab navigation
-map('n', '<c-h>', ':tabp<cr>', options)
-map('n', '<c-l>', ':tabn<cr>', options)
+map('n', '<c-h>', ':tabp<cr>', { noremap = true })
+map('n', '<c-l>', ':tabn<cr>', { noremap = true })
 
 -- split panes
-map('n', '<leader>sh', ':sp<cr>', options)
-map('n', '<leader>sv', ':vsp<cr>', options)
-map('n', '<leader>sd', '<c-w><c-o>', options)
+map('n', '<leader>sh', ':sp<cr>', { noremap = true })
+map('n', '<leader>sv', ':vsp<cr>', { noremap = true })
+map('n', '<leader>sd', '<c-w><c-o>', { noremap = true })
 
 -- new tab
-map('n', '<leader>t', ':tabnew<cr>', options)
+map('n', '<leader>t', ':tabnew<cr>', { noremap = true })
 
 -- diff remaps
-map('n', '<leader>sp', ':diffput<cr>', options)
-map('v', '<leader>sp', ':diffput<cr>', options)
-map('n', '<leader>sg', ':diffget<cr>', options)
-map('v', '<leader>sg', ':diffget<cr>', options)
+map('n', '<leader>sp', ':diffput<cr>', { noremap = true })
+map('v', '<leader>sp', ':diffput<cr>', { noremap = true })
+map('n', '<leader>sg', ':diffget<cr>', { noremap = true })
+map('v', '<leader>sg', ':diffget<cr>', { noremap = true })
 
 -- too much thinking for ^ and $
-map('n', '<leader>[', '^', options)
-map('n', '<leader>]', '$', options)
+map('n', '<leader>[', '^', { noremap = true })
+map('n', '<leader>]', '$', { noremap = true })
 
 -- the system clipboard (tm)
-map('n', '<leader>`', '"*', options)
+map('n', '<leader>`', '"*', { noremap = true })
 
 -- qflist remaps
-map('n', '<c-left>', ':lprev<cr>zz', options)
-map('n', '<c-right>', ':lnext<cr>zz', options)
-map('n', '<c-up>', ':cprev<cr>zz', options)
-map('n', '<c-down>', ':cnext<cr>zz', options)
+map('n', '<c-left>', ':lprev<cr>zz', { noremap = true })
+map('n', '<c-right>', ':lnext<cr>zz', { noremap = true })
+map('n', '<c-up>', ':cprev<cr>zz', { noremap = true })
+map('n', '<c-down>', ':cnext<cr>zz', { noremap = true })
