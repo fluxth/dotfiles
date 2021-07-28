c29cb7214b9fca80021e88ff17a372f7cea3c689
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-07-28 16:40:15 +0700

    make: add meslo font to archlinux install dependencies

--- scripts/install_archlinux.sh
+++ scripts/install_archlinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -Syyu neovim alacritty mpv zsh tmux rustup nodejs npm python-pip ttf-meslo-nerd-font-powerlevel10k
+sudo pacman --noconfirm -Syu base-devel neovim alacritty mpv zsh tmux rustup nodejs npm python-pip ttf-meslo-nerd-font-powerlevel10k
