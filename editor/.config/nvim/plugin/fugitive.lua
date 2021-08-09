de72d2e3e4feff91dbd939e672df723823277428
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-08-09 19:25:51 +0700

    nvim: fix git push binding not showing up by making it non-silent

--- editor/.config/nvim/plugin/fugitive.lua
+++ editor/.config/nvim/plugin/fugitive.lua
@@ -5,4 +5,4 @@ local options = { noremap = true, silent = true }
 map('n', '<leader>gg', ':G<cr>', options)
 map('n', '<leader>gs', ':G<cr>', options)
 map('n', '<leader>gd', ':Gvdiffsplit<cr>', options)
-map('n', '<leader>gp', ':Git push', options)
+map('n', '<leader>gp', ':Git push', { noremap = true })
