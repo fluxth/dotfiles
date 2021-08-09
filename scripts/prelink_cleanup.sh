adae61a553c32fc9cbdbf933929274fbdd3a55c8
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-08-09 14:24:53 +0700

    install: move chsh to after .zshrc check

--- scripts/prelink_cleanup.sh
+++ scripts/prelink_cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 # Install oh-my-zsh
-chsh -s $(which zsh) $(whoami)
-
 if [[ -f "$HOME/.zshrc" ]]; then
     echo "Error: Existing ~/.zshrc found, please check and delete it before continuing."
     exit 1
 fi
+
+chsh -s $(which zsh) $(whoami)
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --keep-zshrc --unattended"
 rm ~/.zshrc
