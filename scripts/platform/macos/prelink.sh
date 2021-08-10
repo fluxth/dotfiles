b9ef3fbfe16818fd32d96969e6f06ed8831712d3
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-08-10 21:55:33 +0700

    install: make homebrew avaliable in $PATH after fresh install

--- scripts/platform/macos/prelink.sh
+++ scripts/platform/macos/prelink.sh
@@ -3,4 +3,6 @@
 if ! command -v brew &> /dev/null; then
     echo "Homebrew not found, installing..."
     curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+    export PATH=/opt/homebrew/bin:/usr/local/bin:$PATH
 fi
