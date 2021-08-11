5797bb33ede60c87a8fd262f055db1b3fb75b6bb
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-08-11 16:09:44 +0700

    nvim: add sh plugin to coc and treesitter

--- editor/.config/nvim/plugin/treesitter.lua
+++ editor/.config/nvim/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
-    "c", "cpp", "python", "rust", "toml", "dockerfile", "yaml", "lua",
+    "c", "cpp", "python", "rust", "toml", "dockerfile", "yaml", "lua", "bash",
     "javascript", "typescript", "tsx", "css", "scss", "html", "json", "jsonc"
   },
   highlight = {
