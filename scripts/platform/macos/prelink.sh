86a58ee59d915b8f01e44a2fa48e286048c8a9a8
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-08-12 14:57:31 +0700

    install: fix curl not running homebrew install script

--- scripts/platform/macos/prelink.sh
+++ scripts/platform/macos/prelink.sh
@@ -2,7 +2,7 @@
 
 if ! command -v brew &> /dev/null; then
     echo "Homebrew not found, installing..."
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     export PATH=/opt/homebrew/bin:/usr/local/bin:$PATH
 fi
