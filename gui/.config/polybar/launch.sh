28ca8299b9393e0ea393df0e0ef44e2a96cb78ef
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-10-22 18:06:15 +0700

    Add small bar to polybar
    Very rough, clean up later.

--- gui/.config/polybar/launch.sh
+++ gui/.config/polybar/launch.sh
@@ -9,16 +9,18 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
     monitor_count="$(xrandr -q | grep -w connected -c)"
     if [[ "$monitor_count" == "1" ]]; then
-        MONITOR=$m TRAY_POS=right polybar --reload main &
+        MONITOR=$m TRAY_POS=right polybar --reload small &
     else
         for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
             # Make tray appear on HDMI monitor
-            local traypos=''
+            bar=small
+            traypos=''
             if [[ "$m" == "HDMI1" ]]; then
                 traypos=right
+                bar=large
             fi
 
-            MONITOR=$m TRAY_POS=$traypos polybar --reload main &
+            MONITOR=$m TRAY_POS=$traypos polybar --reload $bar &
         done
     fi
 else
