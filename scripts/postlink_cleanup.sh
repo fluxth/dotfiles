46e7cd44b90903471827092f7545c0270ae3d883
Thitat Auareesuksakul <thitat@fluxthitat.me>
2021-11-14 15:45:10 +0700

    Update coc

--- scripts/postlink_cleanup.sh
+++ scripts/postlink_cleanup.sh
@@ -9,6 +9,6 @@ ln -s $(which youtube-dl) ~/.config/mpv/youtube-dl
 # Install neovim prerequisites
 pip3 install neovim
 
-cd ~/.config/coc/extensions && npm i
+cd ~/.config/coc/extensions && npm install --ignore-scripts --no-lockfile --production --legacy-peer-deps
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -u ~/.config/nvim/lua/plugins.lua --noplugin -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
