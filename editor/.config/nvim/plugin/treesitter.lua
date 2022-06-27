c090a438256d3fad04278bac0109aaf2d4b903a0
Thitat Auareesuksakul <thitat@flux.ci>
2022-06-27 17:47:07 +0700

    nvim: ensure go and svelte is installed in treesitter

--- editor/.config/nvim/plugin/treesitter.lua
+++ editor/.config/nvim/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = {
-    "c", "cpp", "python", "rust", "toml", "dockerfile", "yaml", "lua", "bash",
-    "javascript", "typescript", "tsx", "css", "scss", "html", "json", "jsonc"
+    "c", "cpp", "python", "rust", "toml", "dockerfile", "yaml", "lua", "bash", "go",
+    "javascript", "typescript", "tsx", "css", "scss", "html", "json", "jsonc", "svelte"
   },
   highlight = {
     enable = true
